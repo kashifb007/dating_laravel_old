c17d7bb589f3f14b4149761241b8371f77714d9f
Kashif Bhatti <kash@dreamsites.co.uk>
2025-10-30 13:40:17 +0000

    Create FetchImagesInterface.php

---
+++ app/Interfaces/FetchImagesInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Interface FetchImagesInterface
+ * Author: Kashif Bhatti
+ * 10/10/2025
+ */
+
+namespace App\Interfaces;
+
+interface FetchImagesInterface
+{
+    public function fetchImages();
+}
